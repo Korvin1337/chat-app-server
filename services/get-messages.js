ba00c75fbaefe3977a93f791b29bbef4e79fb9f6
Robin Blom <robin.blom@hotmail.se>
2022-10-07 15:11:53 +0200

    Fixed bug in SQL query

--- services/get-messages.js
+++ services/get-messages.js
@@ -1,34 +1,34 @@
-let axios = require('axios')
+let axios = require("axios");
 
 function getMessages(room) {
-    const dbUrl = process.env.HARPER_URL
-    const dbPw = process.env.HARPER_PW
-    if (!dbUrl || !dbPw) return null
+  const dbUrl = process.env.HARPER_URL;
+  const dbPw = process.env.HARPER_PW;
+  if (!dbUrl || !dbPw) return null;
 
-    let data = JSON.stringify({
-        operation: 'sql',
-        sql: `SELECT * FROM realtime_chat_app.messages WHERE room = '${room}' LIMIT 20`,
-    })
+  let data = JSON.stringify({
+    operation: "sql",
+    sql: `SELECT * FROM realtime_chat_app.messages WHERE room = '${room}' ORDER BY __createdtime__ DESC LIMIT 20`,
+  });
 
-    let config = {
-        method: 'post',
-        url: dbUrl,
-        headers: {
-            'Content-Type': 'application/json',
-            Authorization: dbPw,
-        },
-        data: data,
-    }
+  let config = {
+    method: "post",
+    url: dbUrl,
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: dbPw,
+    },
+    data: data,
+  };
 
-    return new Promise((resolve, reject) => {
-        axios(config)
-            .then(function (response) {
-                resolve(JSON.stringify(response.data))
-            })
-            .catch(function (error) {
-                reject(error)
-            })
-    })
+  return new Promise((resolve, reject) => {
+    axios(config)
+      .then(function (response) {
+        resolve(JSON.stringify(response.data));
+      })
+      .catch(function (error) {
+        reject(error);
+      });
+  });
 }
 
-module.exports = getMessages
+module.exports = getMessages;
